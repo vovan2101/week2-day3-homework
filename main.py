d4f0b66e8b60b6ee6519c1dfbe305eb3bc263611
Vladimir <veliseykin2000@gmail.com>
2022-07-19 22:10:47 -0700

    /

---
+++ main.py
@@ -0,0 +1,22 @@
+# exersise 1
+
+l_1 = [1,11,14,5,8,9]
+new_list = []
+
+for num in l_1:
+    if num <= 10:
+        new_list.append(num)
+print(new_list)
+
+
+
+
+# exersise 2
+
+names_1 = ['connor', 'connor', 'bob', 'connor', 'evan', 'max', 'evan', 'bob', 'kevin']
+names_2 = []
+
+for i in names_1:
+    if i >= 4:
+        names_2.append(i)
+print(names_2)
